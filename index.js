8c84e3661e93361a0b6abfb570d8ed5726656386
joycewangui <joycewangui590@gmail.com>
2024-07-02 22:28:06 +0700

    adds indx.js

--- index.js
+++ index.js
@@ -1,32 +1,23 @@
-
-const returnFirstTwoDrivers = function(drivers) {
+ const returnFirstTwoDrivers = function (drivers){
+    (drivers['Tim', 'Tom', 'Tony', 'Ted', 'Tilda'])
     return drivers.slice(0, 2);
-};
-  
-const returnLastTwoDrivers= function(drivers){
+ }
+
+const returnLastTwoDrivers = function (drivers){
+    (drivers['Tim', 'Tom', 'Tony', 'Ted', 'Tilda'])
     return drivers.slice(-2);
-};
-  
-
+}
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
-  
-  
-const createFareMultiplier = function(multiplier) {
-    return function(fare) {
-    return fare * multiplier;
+
+const createFareMultiplier = function (multiplier){
+    return function(fare){
+        return fare * multiplier;
     };
-};
+}
+const fareDoubler = createFareMultiplier (2);
   
-  
-const fareDoubler = createFareMultiplier(2);
-  
+const fareTripler = createFareMultiplier (3);
 
-const fareTripler = createFareMultiplier(3);
-  
- 
-const selectDifferentDrivers = function(arrayOfDrivers, driverFunction) {
+const selectDifferentDrivers = function (arrayOfDrivers, driverFunction){
     return driverFunction(arrayOfDrivers);
-};
-  
-  
-
+}
